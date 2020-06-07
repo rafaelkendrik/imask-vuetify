5c13c34e613c45a9d801cb3d145a49e31e9b0d7d
Rafael Kendrik <rafaelkendrik@gmail.com>
2020-06-07 20:04:33 -0300

    add ImaskField component

---
+++ components/pug/ImaskField.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  v-text-field(
+    ref="field"
+    :value="value"
+    @input="input"
+    @blur="checkNormalize"
+    v-bind="$attrs"
+  )
+</template>
+
+<script>
+  import Imask from 'imask'
+
+  const EVENT = 'input'
+
+  let element = {}
+  let masked = {}
+
+  export default {
+    name: 'ImaskField',
+
+    model: {
+      prop: 'value',
+      event: EVENT
+    },
+
+    props: {
+      value: {
+        type: [String, Number],
+        required: false,
+        default: String()
+      },
+
+      mask: {
+        type: Object,
+        required: true
+      }
+    },
+
+    methods: {
+      init () {
+        element = this.$refs.field.$el.querySelector('input')
+        masked = Imask(element, this.mask)
+      },
+
+      input (value) {
+        this.$emit(EVENT, value)
+      },
+
+      checkNormalize () {
+        const isNormalized = (element.value === masked.value)
+
+        if (!isNormalized) {
+          element.value = masked.value
+          element.dispatchEvent(new Event(EVENT))
+        }
+      }
+    },
+
+    mounted () {
+      this.init()
+    }
+  }
+</script>
